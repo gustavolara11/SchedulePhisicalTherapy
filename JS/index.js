a8b5d89afe1d1625ad6caf804994b22d92fb1fd8
Gustavo Lara <gustavex2007@gmail.com>
2024-11-12 12:50:14 -0300

    fix wsl change

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,11 @@
-var ulMessages = document.querySelector("ul#ul_messages");
-var newMessageContainer = document.querySelector("div.newMessageContainer");
+async function main() {
+  var ulMessages = document.querySelector("ul#ul_messages");
+  var newMessageContainer = document.querySelector("div.newMessageContainer");
 
 // Patients names on Select
 async function renderSelect() {
   data = { operation: "select" };
-  const response = await fetch("../api/endpoint.php", {
+  const response = await fetch("api/endpoint.php", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -12,7 +13,7 @@ async function renderSelect() {
     body: JSON.stringify(data),
   });
   const jsonData = await response.json();
-
+  
   var list = document.querySelector("select#patient_name");
   list.innerHTML = "";
   for (i = 0; i < jsonData.length; i++) {
@@ -20,3 +21,5 @@ async function renderSelect() {
   }
 }
 await renderSelect();
+}
+main();
